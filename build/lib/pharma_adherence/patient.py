30bfe20667873dda5ee8a5eb5e12db81cb02e2cb
theosyonactual <theosyonactual@gmail.com>
2026-06-05 03:11:48 -0700

    Update patient.py

--- build/lib/pharma_adherence/patient.py
+++ build/lib/pharma_adherence/patient.py
@@ -4,7 +4,9 @@ class PatientAdherenceProfile:
     def __init__(self, patient_id, df: pd.DataFrame):
         self.patient_id = patient_id
         self.df = df.sort_values("fill_date")
+'''This creates the Patient Adherecne Profile class, and defines the patient id variable. Its values are sorted by the 'fill date' before being stored. Note this uses a pandas dataframe in pd.Dataframe'''
 
+    '''The Todo comments articulate what each of the following lines of code do'''
     def total_fills(self):
         #TODO: Calculate and return the total number of fills?
         return len(self.df)
@@ -34,4 +36,4 @@ class PatientAdherenceProfile:
             "pdc": self.calculate_pdc(),
             "adherent": self.is_adherent(),
         }
-        
+        '''The summary function allows for the output of several pieces of data with a single query. In this instance, it is returning a specific patient's entire drug info page including variables like copay, total fills, etc. when queried for a patient's profile'''
